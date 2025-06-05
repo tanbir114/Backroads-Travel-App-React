f62635b87959ee6b46ef1a809ec28fb4ac52cdbe
tanbir114 <tanbir@iut-dhaka.edu>
2025-06-05 21:32:09 +0600

    final changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Tours from "./components/Tours";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 
 function App() {
   return (
